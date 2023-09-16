c84806c82d64f419687bae47cbf0bd80a194ea66
Shane Winter <shanewin@gmail.com>
2023-09-15 23:22:05 -0400

    Edited Settings for PythonAnywhere

--- user_management/settings.py
+++ user_management/settings.py
@@ -101,12 +101,14 @@ WSGI_APPLICATION = "user_management.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default="sqlite:///" + str(BASE_DIR / "db.sqlite3")
+    )
 }
+
 
 
 # Password validation
